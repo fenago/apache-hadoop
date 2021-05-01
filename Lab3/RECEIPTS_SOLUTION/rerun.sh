53eb210c1a1991ac80d209ba35fc5c04bee664c2
fenago <socrates73@gmail.com>
2021-05-01 18:25:08 +0500

    labs added

--- Lab3/RECEIPTS_SOLUTION/rerun.sh
+++ Lab3/RECEIPTS_SOLUTION/rerun.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-export HADOOP_HOME=/home/hdoop/hadoop-2.5.1
+export HADOOP_HOME=/home/hdoop/hadoop-2.7.0
 export LD_LIBRARY_PATH=$HADOOP_HOME/lib/native/Linux-amd64-64
 export CLASSPATH=$HADOOP_HOME/share/hadoop/mapreduce/*:$HADOOP_HOME/share/hadoop/mapreduce/lib/*:$HADOOP_HOME/share/hadoop/common/lib/*:$HADOOP_HOME/share/hadoop/common/*:$HADOOP_HOME/share/hadoop/tools/lib/*:$HADOOP_HOME/share/hadoop/yarn/*:$HADOOP_HOME/share/hadoop/yarn/lib/*
 export HADOOP_CLASSPATH=$CLASSPATH
 
 USER=`whoami`
 
-rm -rf /home/$USER/3/RECEIPTS_SOLUTION/OUT
-hadoop jar Receipts.jar Receipts.ReceiptsDriver /home/$USER/3/RECEIPTS_SOLUTION/DATA/receipts.txt /home/$USER/3/RECEIPTS_SOLUTION/OUT 
+rm -rf /home/$USER/Lab3/RECEIPTS_LAB/OUT
+
+hadoop fs -mkdir -p /home/$USER/Lab3/RECEIPTS_LAB/DATA/receipts.txt
+hadoop fs -put /home/$USER/Lab3/RECEIPTS_LAB/DATA/receipts.txt /home/$USER/Lab3/RECEIPTS_LAB/DATA
+
+hadoop jar Receipts.jar Receipts.ReceiptsDriver /home/$USER/Lab3/RECEIPTS_LAB/DATA/receipts.txt /home/$USER/Lab3/RECEIPTS_LAB/OUT 
+
+hadoop fs -copyToLocal  /home/hdoop/Lab3/RECEIPTS_LAB/OUT /home/hdoop/Lab3/RECEIPTS_LAB/OUT
+
