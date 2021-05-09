64744cbdc208b52078dbe72ca54955b6aabbe393
fenago <socrates73@gmail.com>
2021-05-09 20:03:40 +0500

    added

--- Lab5/VOTER_SOLUTION/rerun.sh
+++ Lab5/VOTER_SOLUTION/rerun.sh
@@ -12,14 +12,11 @@ echo "Deleting OUT folder from local and hadoop filesystem (if exists)"
 rm -rf /home/$USER/Lab5/VOTER_SOLUTION/OUT
 hadoop fs -rm -r /home/$USER/Lab5/VOTER_SOLUTION/
 
-hadoop fs -mkdir -p /home/$USER/Lab5/VOTER_SOLUTION/DATA
-hadoop fs -put /home/$USER/Lab5/VOTER_SOLUTION/DATA /home/$USER/Lab5/VOTER_SOLUTION/DATA
-
-
-ARGS=$1
 
 hadoop fs -mkdir -p /home/$USER/Lab5/VOTER_SOLUTION/DATA/
 hadoop fs -put /home/$USER/Lab5/VOTER_SOLUTION/DATA/myvoter.csv /home/$USER/Lab5/VOTER_SOLUTION/DATA
+
+ARGS=$1
 
 hadoop jar Voter.jar Voter.VoterDriver $ARGS /home/$USER/Lab5/VOTER_SOLUTION/DATA/myvoter.csv /home/$USER/Lab5/VOTER_SOLUTION/OUT
 
