ab71207eee762ca0233c6e65324a73a989c63d8d
fenago <socrates73@gmail.com>
2021-05-09 23:49:14 +0500

    added

--- Lab8/STATISTICS_SOLUTION/whole_rerun.sh
+++ Lab8/STATISTICS_SOLUTION/whole_rerun.sh
@@ -6,10 +6,22 @@ export CLASSPATH=$HADOOP_HOME/share/hadoop/mapreduce/*:$HADOOP_HOME/share/hadoop
 
 USER=`whoami`
 
+
+echo "Deleting OUT folder from local and hadoop filesystem (ignore not found warning)"
 rm -rf /home/$USER/Lab8/STATISTICS_SOLUTION/UNIV_OUT
 rm -rf /home/$USER/Lab8/STATISTICS_SOLUTION/STAT_OUT
+
+
+hadoop fs -rm -r /home/$USER/Lab8/STATISTICS_SOLUTION/UNIV_OUT
+hadoop fs -rm -r /home/$USER/Lab8/STATISTICS_SOLUTION/STAT_OUT
+
+
+hadoop fs -mkdir -p /home/$USER/Lab5/VOTER_SOLUTION/DATA/
+hadoop fs -put -f /home/$USER/Lab8/STATISTICS_SOLUTION/DATA/university.txt /home/$USER/Lab8/STATISTICS_SOLUTION/DATA
+
 
 
 hadoop jar WholeJob.jar WholeJob.WholeJobDriver -D var1="verbal" -D var2="math" /home/$USER/Lab8/STATISTICS_SOLUTION/DATA/university.txt /home/$USER/Lab8/STATISTICS_SOLUTION/UNIV_OUT /home/$USER/Lab8/STATISTICS_SOLUTION/STAT_OUT
 
 
+hadoop fs -copyToLocal  /home/$USER/Lab8/STATISTICS_SOLUTION/ /home/$USER/Lab8/STATISTICS_SOLUTION/
